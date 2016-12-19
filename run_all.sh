8c4ba2e8d2c2d27bd0b7e39fb0a925111e274ab3
Aaron Lun <alun@wehi.edu.au>
2016-12-19 11:52:03 +0000

    Updated runner script.

--- run_all.sh
+++ run_all.sh
@@ -6,3 +6,19 @@ echo "knitr::knit('naive_analysis.Rmd')" | R --no-save --vanilla
 echo "knitr::knit('primed_analysis.Rmd')" | R --no-save --vanilla
 echo "knitr::knit('remapping.Rmd')" | R --no-save --vanilla
 
+echo "rmarkdown::render('preprocess.md')" | R --no-save --vanilla
+echo "rmarkdown::render('overall_analysis.md')" | R --no-save --vanilla
+echo "rmarkdown::render('naive_analysis.md')" | R --no-save --vanilla
+echo "rmarkdown::render('primed_analysis.md')" | R --no-save --vanilla
+echo "rmarkdown::render('remapping.md')" | R --no-save --vanilla
+
+targetdir=~/Dropbox/primed_vs_naive_ESC_analysis/latest_analysis/
+if [ -e $targetdir ]
+then
+    cp *.html $targetdir
+    cp -r results-* $targetdir
+else 
+    echo "'$targetdir' does not exist!"
+    exit 1    
+fi
+
