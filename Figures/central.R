5fe645ff5c5baa7a05735cba60c1f7a55a8a552a
Tobias Meßmer <tobiasmessmer@Tobiass-MacBook-Pro-3.local>
2018-07-17 11:23:28 +0200

    Moved to separate figure folder.

---
+++ Figures/central.R
@@ -0,0 +1,8 @@
+# Values required for all figure-making scripts.
+
+figdir <- "Figures"
+dir.create(figdir, showWarnings = FALSE)
+primed.col <- "#0072B2"
+naive.col <- "#e79f00"
+trans.col <- "#009E73"
+
