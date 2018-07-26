2ab81e6cbdcbce6db4b454dc77b896e557e227b3
Tobias Meßmer <tobiasmessmer@Tobiass-MacBook-Pro-3.local>
2018-07-26 09:28:24 +0200

    Moved execution/download files into separate folder.

---
+++ analysis/make_figures.sh
@@ -0,0 +1,18 @@
+Rscript Figures/Figure1.R
+Rscript Figures/Figure2.R
+Rscript Figures/Figure3.R
+Rscript Figures/Figure4.R
+
+
+
+targetdir=~/Dropbox/primed_vs_naive_ESC_analysis/latest_analysis/
+if [ -e $targetdir ]
+then
+    cp *.html $targetdir
+    cp -r results-* $targetdir
+    cp -r Figures $targetdir
+else 
+    echo "'$targetdir' does not exist!"
+    exit 1    
+fi
+
---
+++ analysis/reanalyze_g1.sh
@@ -0,0 +1,32 @@
+# This script generates new Rmarkdown files using only G1-related cells 
+# in naive_analysis.Rmd and primed_analysis.Rmd. The idea is to check
+# that the same conclusions hold when the effect of cell cycle is removed.
+
+for mode in naive primed
+do
+    newfile=g1_${mode}_analysis.Rmd
+    cat ${mode}_analysis.Rmd | sed "s/figure-${mode}/figure-g1_${mode}/g" \
+        | sed -r 's/(sce <- readRDS\("sce_all.rds"\))/\1\nsce <- sce[,sce$phase=="G1"]/' \
+        | sed "s/resdir <- \"results-${mode}\"/resdir <- \"results-g1_${mode}\"/" \
+        | sed "s/saveRDS(sce_${mode}, file=\"sce_${mode}.rds\")//" \
+        | sed "s/embryonic stem cells: ${mode}-only analysis/embryonic stem cells: G1-only, ${mode}-only analysis/" \
+        | sed -r "s/(This is more sensitive than doing so on the entire data set, where the naive\/primed differences dominate.)/\1\nWe subset to only use G1 cells, to check that the cell cycle phase does not have any effect on the analysis./" \
+        | sed "s/We save our object for later use and/We/" > ${newfile}
+
+    if [[ ${mode} == "naive" ]]
+    then
+        top="We also check the cell cycle phase for each of the three clusters."
+        bottom="We also examine the location of cluster 3 on the overall PCA plot."
+        sed "/^${top}/,/^${bottom}/ { /^${bottom}/b; d }" ${newfile} > .tmp.Rmd
+    else 
+        top="### Effect of the cell cycle"
+        bottom="# Wrapping up"
+        sed "/^${top}/,/^${bottom}/ { /^${bottom}/b; d }" ${newfile} > .tmp.Rmd
+    fi
+    mv .tmp.Rmd ${newfile}
+ done
+
+
+
+    
+    
