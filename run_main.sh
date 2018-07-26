6edeb7a180ac4b2b83a5b77a1a1719c18acae390
Tobias Meßmer <tobiasmessmer@Tobiass-MacBook-Pro-3.local>
2018-07-26 09:40:57 +0200

    Moved back the files to run the scripts.

---
+++ run_main.sh
@@ -0,0 +1,25 @@
+set -e
+set -u
+
+echo "rmarkdown::render('preprocess.Rmd', output_file = 'preprocess.html')" | R --no-save --vanilla
+echo "rmarkdown::render('overall_analysis.Rmd', output_file = 'overall_analysis.html')" | R --no-save --vanilla
+echo "rmarkdown::render('naive_analysis.Rmd', output_file = 'naive_analysis.html')" | R --no-save --vanilla
+echo "rmarkdown::render('transition_analysis.Rmd', output_file = 'transition_analysis.html')" | R --no-save --vanilla
+echo "rmarkdown::render('primed_analysis.Rmd', output_file = 'primed_analysis.html')" | R --no-save --vanilla
+echo "rmarkdown::render('diff.Rmd', output_file = 'diff.html')" | R --no-save --vanilla
+echo "rmarkdown::render('correlations.Rmd', output_file = 'correlations.html')" | R --no-save --vanilla
+echo "rmarkdown::render('remapping.Rmd', output_file = 'remapping.html')" | R --no-save --vanilla
+echo "rmarkdown::render('qualitycheck.Rmd', output_file = 'qualitycheck.html')" | R --no-save --vanilla
+
+targetdir="~/Desktop/Ergebnisse
+"
+if [ -e $targetdir ]
+then
+    cp *.html $targetdir
+    cp -r results-* $targetdir
+    cp -r Figures $targetdir
+else 
+    echo "'$targetdir' does not exist!"
+    exit 1    
+fi
+
