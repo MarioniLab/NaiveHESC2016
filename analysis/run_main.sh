31e29a6c5d9b00e7f2f1a4b0970a3856ae188c54
Tobias Meßmer <tobiasmessmer@Tobiass-MacBook-Pro-3.local>
2018-09-14 11:24:26 +0200

    Adjusted file paths.

--- analysis/run_main.sh
+++ analysis/run_main.sh
@@ -1,13 +1,13 @@
 set -e
 set -u
 
-echo "rmarkdown::render('analysis/preprocess.Rmd', output_file = 'preprocess.html')" | R --no-save --vanilla
-echo "rmarkdown::render('analysis/overall_analysis.Rmd', output_file = 'overall_analysis.html')" | R --no-save --vanilla
-echo "rmarkdown::render('analysis/naive_analysis.Rmd', output_file = 'naive_analysis.html')" | R --no-save --vanilla
-echo "rmarkdown::render('analysis/transition_analysis.Rmd', output_file = 'transition_analysis.html')" | R --no-save --vanilla
-echo "rmarkdown::render('analysis/primed_analysis.Rmd', output_file = 'primed_analysis.html')" | R --no-save --vanilla
-echo "rmarkdown::render('analysis/diff.Rmd', output_file = 'diff.html')" | R --no-save --vanilla
-echo "rmarkdown::render('analysis/correlations.Rmd', output_file = 'correlations.html')" | R --no-save --vanilla
-echo "rmarkdown::render('analysis/remapping.Rmd', output_file = 'remapping.html')" | R --no-save --vanilla
-echo "rmarkdown::render('analysis/qualitycheck.Rmd', output_file = 'qualitycheck.html')" | R --no-save --vanilla
+echo "rmarkdown::render('preprocess.Rmd', output_file = 'preprocess.html')" | R --no-save --vanilla
+echo "rmarkdown::render('overall_analysis.Rmd', output_file = 'overall_analysis.html')" | R --no-save --vanilla
+echo "rmarkdown::render('naive_analysis.Rmd', output_file = 'naive_analysis.html')" | R --no-save --vanilla
+echo "rmarkdown::render('transition_analysis.Rmd', output_file = 'transition_analysis.html')" | R --no-save --vanilla
+echo "rmarkdown::render('primed_analysis.Rmd', output_file = 'primed_analysis.html')" | R --no-save --vanilla
+echo "rmarkdown::render('diff.Rmd', output_file = 'diff.html')" | R --no-save --vanilla
+echo "rmarkdown::render('correlations.Rmd', output_file = 'correlations.html')" | R --no-save --vanilla
+echo "rmarkdown::render('remapping.Rmd', output_file = 'remapping.html')" | R --no-save --vanilla
+echo "rmarkdown::render('qualitycheck.Rmd', output_file = 'qualitycheck.html')" | R --no-save --vanilla
 
